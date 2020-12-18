c72f25c2db3933b950f1cc72eda9d02376f499a3
hau <ngtranhau@gmail.com>
2020-12-18 17:21:21 +0700

    refactor: split color func from runGoTest

--- main.go
+++ main.go
@@ -33,54 +33,57 @@ func runGoTest() int {
 	cmd.Stdout = outWriter
 	cmd.Stderr = errWriter
 
-	go func() {
-		defer outReader.Close()
-
-		scanner := bufio.NewScanner(outReader)
-
-		for scanner.Scan() {
-			line := scanner.Text()
-			trimmedLine := strings.TrimSpace(line)
-
-			if strings.HasPrefix(trimmedLine, "--- PASS") ||
-				strings.HasPrefix(line, "PASS") ||
-				strings.HasPrefix(line, "ok") {
-				color.Green("%s\n", line)
-				continue
-			}
-
-			if strings.HasPrefix(trimmedLine, "--- FAIL") ||
-				strings.HasPrefix(line, "FAIL") {
-				color.Red("%s\n", line)
-				continue
-			}
-
-			fmt.Println(line)
-		}
-
-		if err := scanner.Err(); err != nil {
-			log.Printf("scanner error: %s", err)
-		}
-	}()
-
-	go func() {
-		defer errReader.Close()
-
-		scanner := bufio.NewScanner(errReader)
-
-		for scanner.Scan() {
-			line := scanner.Text()
-			color.Red("%s\n", line)
-		}
-
-		if err := scanner.Err(); err != nil {
-			log.Printf("scanner error: %s", err)
-		}
-	}()
+	go colorOutputReader(outReader)
+	go colorErrorReader(errReader)
 
 	if err := cmd.Run(); err != nil {
 		return 1
 	}
 
 	return 0
+}
+
+func colorOutputReader(reader io.ReadCloser) {
+	defer reader.Close()
+
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		trimmedLine := strings.TrimSpace(line)
+
+		if strings.HasPrefix(trimmedLine, "--- PASS") ||
+			strings.HasPrefix(line, "PASS") ||
+			strings.HasPrefix(line, "ok") {
+			color.Green("%s\n", line)
+			continue
+		}
+
+		if strings.HasPrefix(trimmedLine, "--- FAIL") ||
+			strings.HasPrefix(line, "FAIL") {
+			color.Red("%s\n", line)
+			continue
+		}
+
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("scanner error: %s", err)
+	}
+}
+
+func colorErrorReader(reader io.ReadCloser) {
+	defer reader.Close()
+
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		color.Red("%s\n", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("scanner error: %s", err)
+	}
 }
